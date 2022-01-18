007284bdd7da1f09979265a0e994c72d2e921ed9
Parth Shukla <31998380+ParthShuklaa@users.noreply.github.com>
2022-01-18 09:44:00 +0530

    Updating Day 1 Files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Namespaces are written with using keyword 
+//namespaces are helpfulr for creating ref of Framework Class Library (FCL)
+
+namespace Demo_HelloWorld_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I am Feeling Lucky.....");
+            Console.Write("Its freezing cold in Delhi....");
+           
+
+            Console.ReadLine();
+            //keyboard Short cut for running code in VS is Ctrl+f5
+        }
+    }
+}
